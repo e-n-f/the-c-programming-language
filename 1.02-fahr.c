58cceb2f7841283c3b0344f111c0112e1f8e0c66
Eric Fischer <enf@pobox.com>
2018-01-06 15:37:02 -0800

    Fahrenheit-Celsius table

---
+++ 1.02-fahr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+
+// Print Fahrenheit-Celsius table
+// for fahr = 0, 20, ..., 300
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	int fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;	// lower limit of temperature table
+	upper = 300;	// upper limit
+	step = 20;	// step size
+
+	fahr = lower;
+	while (fahr <= upper) {
+		celsius = 5 * (fahr - 32) / 9;
+		wprintf(L"%d\t%d\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+
+	return 0;
+}

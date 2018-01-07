4b60fc2762458201ae33fed3e960fa0c8fda82d9
Eric Fischer <enf@pobox.com>
2018-01-07 00:33:46 -0800

    Word count with ctype

---
+++ 1.05.04-wordcount-ctype.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <wctype.h>
+
+// Count words
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	bool within_word = false;
+	wint_t c;
+	size_t nl, nw, nc;
+
+	nl = nw = nc = 0;
+	while ((c = getwchar()) != WEOF) {
+		nc++;
+
+		if (c == L'\n') {
+			nl++;
+		}
+
+		if (iswspace(c)) {
+			within_word = false;
+		} else if (!within_word) {
+			within_word = true;
+			nw++;
+		}
+	}
+
+	wprintf(L"%zu %zu %zu\n", nl, nw, nc);
+	return 0;
+}

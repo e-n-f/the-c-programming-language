2f3bb501fe0e47ade2f3bf82235009e8e5c02672
Eric Fischer <enf@pobox.com>
2018-01-07 00:52:37 -0800

    Exponentiation via function call

---
+++ 1.07-power.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+
+int power(int base, size_t n);
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	size_t i;
+	for (i = 0; i < 10; i++) {
+		wprintf(L"%zu %d %d\n", i, power(2, i), power(-3, i));
+	}
+
+	return 0;
+}
+
+int power(int base, size_t n) {
+	int p;
+	size_t i;
+
+	p = 1;
+	for (i = 0; i < n; i++) {
+		p = p * base;
+	}
+
+	return p;
+}

633b31bb6aa928c6378411a331aebbda12df6c29
Eric Fischer <enf@pobox.com>
2018-01-07 00:55:28 -0800

    Counting down

---
+++ 1.08-power2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+
+int power(int base, size_t n);
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	size_t i;
+	for (i = 0; i < 10; i++) {
+		wprintf(L"%zu %d %d\n", i, power(2, i), power(-3, i));
+	}
+
+	return 0;
+}
+
+int power(int base, size_t n) {
+	int p;
+
+	p = 1;
+	for (; n > 0; n--) {
+		p = p * base;
+	}
+
+	return p;
+}
